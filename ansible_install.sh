a520592069772d3252f16895f51c6b9a47dc883f
mataborg <97360301+mataborg@users.noreply.github.com>
2022-02-14 11:50:42 +0000

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -3,8 +3,8 @@
 install_ansible() {
 
     apt install ansible -y
-    git clone --depth 1 https://github.com/mataborg/labwrecker /home/eagle/.ansible/
-    git clone --depth 1 https://github.com/mataborg/ansible-role-proxmox_autoinstall /home/eagle/.ansible/roles/
+    git clone --depth 1 https://github.com/mataborg/labwrecker ~/.ansible/
+    git clone --depth 1 https://github.com/mataborg/ansible-role-proxmox_autoinstall ~/.ansible/roles/
 
 }
 
