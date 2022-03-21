3f3bbe433654704c43ebaa121b1912bb48644799
Logan Cowan <97360301+mataborg@users.noreply.github.com>
2022-03-21 14:44:36 +0000

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -6,8 +6,9 @@ install_ansible() {
     git clone --depth 1 https://github.com/mataborg/labwrecker ~/.ansible/
     git clone --depth 1 https://github.com/mataborg/ansible-role-proxmox_autoinstall ~/.ansible/roles/
     ssh-keygen
-    ssh-copy-id eagle@192.168.2.25 -p 49469
-    ssh-copy-id eagle@192.168.2.108 -p 49469
+    chmod 700 /home/eagle/.ssh/id_rsa
+    ssh-copy-id -p 49469 eagle@192.168.2.25
+    ssh-copy-id -p 49469 eagle@192.168.2.108
 
 }
 
