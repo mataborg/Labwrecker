4c22ff4284e8f9bb826917081d873b05ae961805
Logan Cowan <97360301+mataborg@users.noreply.github.com>
2023-08-06 21:36:39 +0100

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -5,17 +5,17 @@ install_ansible() {
     apt install pipx -y
     pipx install --include-deps ansible
     git clone --depth 1 https://github.com/mataborg/labwrecker ~/.ansible/
-    git clone --depth 1 https://github.com/mataborg/ansible-role-proxmox_autoinstall ~/.ansible/roles/
-    ssh-keygen
-    chmod 700 /home/eagle/.ssh/id_rsa
-    ssh-copy-id -p 49469 coganlowan@192.168.2.1
-    ssh-copy-id -p 22 root@192.168.2.11
-    ssh-copy-id -p 22 root@192.168.2.12
-    ssh-copy-id -p 22 root@192.168.2.13
-    ssh-copy-id -p 22 eagle@192.168.2.14
-    ssh-copy-id -p 49469 root@192.168.2.15
-    ssh-copy-id -p 22 eagle@192.168.2.16
-    ssh-copy-id -p 49469 eagle@192.168.2.18
+    git clone --depth 1 https://github.com/mataborg/ansible-role-proxmox_autoinstall ~/.ansible/roles/proxmox_autoinstall
+    # ssh-keygen
+    # chmod 700 /home/eagle/.ssh/id_rsa
+    # ssh-copy-id -p 49469 coganlowan@192.168.2.1
+    # ssh-copy-id -p 22 root@192.168.2.11
+    # ssh-copy-id -p 22 root@192.168.2.12
+    # ssh-copy-id -p 22 root@192.168.2.13
+    # ssh-copy-id -p 22 eagle@192.168.2.14
+    # ssh-copy-id -p 49469 root@192.168.2.15
+    # ssh-copy-id -p 22 eagle@192.168.2.16
+    # ssh-copy-id -p 49469 eagle@192.168.2.18
 
 }
 
